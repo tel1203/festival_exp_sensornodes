285b4f0a596ff58b06f3ec1ef4e77d168f48db26
Teruaki Yokoyama <tel1203@gmail.com>
2017-03-13 11:11:10 +0900

    modify for MQTT transmission

--- pm25_monitor.rb
+++ pm25_monitor.rb
@@ -2,20 +2,21 @@
 # pm25_monitor.rb : getting measurement data about PM2.5 from Sinnei sensor
 #
 # Usage:
-#   $ ruby pm25_monitor.rb 192.168.101.3 Kameoka /run/shm/sensor
-#   $ ruby pm25_monitor.rb 192.168.101.4 Maya    /run/shm/sensor
+# ruby pm25_monitor.rb.rb PM25_sensor Location Data_storage MQTT_server MQTT_port MQTT_topic
+# ruby pm25_monitor.rb 192.168.102.4 Osaka001 /run/shm/sensor festival.ckp.jp 1883 stationsensors/stationsensors_osaka001/pm25
 #
 
 require "./lib_sinnei_pm25.rb"
 require "./lib_sensornode.rb"
 STDOUT.sync = true
 
-if (ARGV[2] != nil) then
-  dir = ARGV[2]
-else
-  dir = "./"
-end
-check_dir(dir)
+sensor_ip = ARGV[0]
+location = ARGV[1]
+dir = ARGV[2]
+mqtt_host = ARGV[3]
+mqtt_port = ARGV[4]
+mqtt_topic = ARGV[5]
+type = "pm25"
 
 # READ DATA
 # measurement time (sec)
@@ -24,35 +25,14 @@ check_dir(dir)
 # humidty (%rh)
 # PM_AD value
 
-s = Sensor_pm25.new(ARGV[0], 10001)
+s = Sensor_pm25.new(sensor_ip, 10001)
 
-begin
-  data = make_data("pm25", ARGV[1], s.send_RD()[1])
-  save_sensordata(data, dir)
+values = Hash.new
+values["pm25"] = s.send_RD()[1]
+data = make_data(type, location, values)
 
-  # MQTT publish
-  host = "210.156.3.153" # festival.ckp.jp
-#  host = "test.mosquitto.org"
-  port = 1883
-  value = data["value"]
-  if (ARGV[1] == "Maya") then
-    # Old topic
-    topic = "stationsensors/stationsensors_MAYA/pm25"
-    mqtt_publish(host, port, topic, value)
-    # New topic @ 2017/03/03
-    topic = "stationsensors/stationsensors_hyogo001/pm25"
-    mqtt_publish(host, port, topic, value)
-  end
+save_sensordata(data, dir)
+mqtt_publish(mqtt_host, mqtt_port, mqtt_topic, s.send_RD()[1])
 
-  if (ARGV[1] == "Kameoka") then
-    # Old topic
-    topic = "stationsensors/stationsensors_KAMEOKA/pm25"
-    mqtt_publish(host, port, topic, value)
-    # New topic @ 2017/03/03
-    topic = "stationsensors/stationsensors_kyoto001/pm25"
-    mqtt_publish(host, port, topic, value)
-  end
-
-rescue
-end  
+p s.send_RD()
 
